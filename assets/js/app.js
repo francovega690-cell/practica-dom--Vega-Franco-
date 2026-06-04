cebd75f78c218810e983312d8979175cb586958c
francovega690-cell <francovega690@gmail.com>
2026-06-04 16:08:25 -0300

    se despliega los heroes

---
+++ assets/js/app.js
@@ -0,0 +1,62 @@
+const personajes = [
+  {
+    id: 1,
+    nombre: "A-Bomb",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/1-a-bomb.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Abe Sapien",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/2-abe-sapien.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Abin Sur",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/3-abin-sur.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Abomination",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/4-abomination.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Abraxas",
+    imagen:
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg",
+  },
+];
+
+const rowContainer = document.querySelector("#rowContainer");
+const formulario = document.querySelector("#formulario");
+const btnAgregarHeroe = document.querySelector("#btnAgregarHeroe");
+const nombreHeroe = document.querySelector("#nombreHeroe");
+const urlHeroe = document.querySelector("#urlHeroe");
+const cargarPersonajes = (arregloDePersonajes) => {
+  rowContainer.innerHTML = "";
+
+  arregloDePersonajes.forEach((personaje) => {
+    rowContainer.innerHTML += `
+      <div class="col-3 my-2" data-id="${personaje.id}">
+        <div class="card" style="width: 10rem">
+          <img
+            src="${personaje.imagen}"
+            class="card-img-top"
+            alt="${personaje.nombre}"
+            style="height: 150px; object-fit: contain"
+          />
+          <div class="card-body">
+            <h5 class="card-title">${personaje.nombre}</h5>
+           
+            <a href="#" class="btn btn-danger btn-eliminar">Eliminar</a>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+};
+cargarPersonajes(personajes);
